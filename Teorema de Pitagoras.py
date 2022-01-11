e1d832961c7399d87fc68bd6c7e7cbd010218e80
PrepED <olindinaprepara@gmail.com>
2022-01-11 10:23:58 -0300

    commit inicial

---
+++ Teorema de Pitagoras.py
@@ -0,0 +1,6 @@
+co = int (input ("Digite o valor do cateto oposto: "))
+ca = int (input ("Digite o valor do cateto adjacente: "))
+
+hipotenusa = int (((co ** 2) + (ca ** 2))**(1/2))
+
+print ("A hipotenusa é:", hipotenusa)
